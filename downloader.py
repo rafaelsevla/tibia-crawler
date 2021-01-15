d08ea8766dc892265640f2d8efbb11c79c2bb98a
Rafael Costa <rafaelsevla@gmail.com>
2021-01-15 19:38:33 -0300

    add structure

---
+++ downloader.py
@@ -0,0 +1,18 @@
+import requests
+
+class Downloader:
+    def __init__(self):
+        self.session = requests.Session()
+
+    def get(self, url, params=None):
+        response = self.session.get(url, params=params, verify=False)
+        response.raise_for_status
+        return response
+
+    def post(self, url, data=None):
+        response = self.session.post(url, data=data, verify=False)
+        response.raise_for_status
+        return response
+
+    def close(self):
+        self.session.close( )
